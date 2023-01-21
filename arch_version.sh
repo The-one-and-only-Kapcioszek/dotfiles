d1beaedf9bbd72a84a4231928feefeb076db723d
Kapicoszek <BartGOKU@onet.pl>
2023-01-21 17:18:48 +0100

    fixed arch_version.sh script a bit(3)

--- arch_version.sh
+++ arch_version.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd ..
 git clone https://github.com/jonaburg/picom
 sudo pacman -S feh dmenu rofi polybar kitty bspwm sxhkd ninja meson
 sudo pacman -S libev uthash libconfig pcre libx11 libxext libxcb pixman dbus
