b376db7a070a62102601eadfd183b101905361c2
Kapicoszek <BartGOKU@onet.pl>
2023-02-02 20:13:29 +0100

    fixed??(2) fedora script

--- fedora_version.sh
+++ fedora_version.sh
@@ -3,7 +3,7 @@
 cd ..
 git clone https://github.com/jonaburg/picom
 sudo dnf install feh dmenu rofi polybar kitty bspwm sxhkd ninja-build meson
-sudo dnf install libxcb libX11 libX11-devel xcb-util-renderutil xcb-util-renderutil-devel libX11-xcb libXext xcb-util libXdamage libXfixes libXinerama glx-utils pixman dbus libconfig-devel mesa-libGL pcre libev libev-devel uthash-devel cmake 
+sudo dnf install libxcb libX11 libX11-devel xcb-util-renderutil xcb-util-image xcb-util-image-devel xcb-util-renderutil-devel libX11-xcb libXext xcb-util libXdamage libXfixes libXinerama glx-utils pixman dbus libconfig-devel mesa-libGL pcre libev libev-devel uthash-devel cmake 
 cd picom
 meson --buildtype=release . build
 ninja -C build
