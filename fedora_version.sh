c42d770eed3ce42ad3f71f7b26f033d316f5d300
Kapicoszek <BartGOKU@onet.pl>
2023-02-02 18:38:32 +0100

    added script for fedora

---
+++ fedora_version.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd ..
+git clone https://github.com/jonaburg/picom
+sudo dnf install feh dmenu rofi polybar kitty bspwm sxhkd ninja-build meson
+sudo dnf install libxcb libx11 libx11-xcb libXext xcb-util libXdamage libXfixes libXinerama glx-utils pixman dbus libconfig-devel mesa-libGL pcre libev-devel uthash-devel
+cd picom
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+cd ..
+cd dotfiles
+cp -r .config $HOME
+cp -r .local $HOME
+cp .fehbg $HOME
+sudo chmod +x $HOME/.config/bspwm/bspwmrc
+sudo chmod +x $HOME/.config/polybar/launch.sh
