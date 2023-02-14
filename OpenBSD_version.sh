c00d5dc58763aa12176cede0f27da164e503021e
Kapicoszek <BartGOKU@onet.pl>
2023-02-14 17:42:53 +0100

    working on OpenBSD script

--- OpenBSD_version.sh
+++ OpenBSD_version.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 
-cd ..
-git clone https://github.com/jonaburg/picom
-doas pkg_add feh dmenu rofi polybar kitty bspwm sxhkd ninja meson
-#pkg_add libx11-dev libx11-xcb-dev libxext-dev xcb libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present0-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-3 libconfig++-dev libgl-dev libpcre++-dev libev-dev uthash-dev
-cd picom
-meson --buildtype=release . build
-ninja -C build
-doas ninja -C build install
-cd ..
-cd dotfiles
+doas pkg_add feh dmenu rofi polybar kitty bspwm sxhkd picom
 cp -r .config $HOME
 cp -r .local $HOME
 cp .fehbg $HOME
